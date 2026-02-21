8f588d45f52fc573ff1666ac284f364fda1e97b4
Emily Ramage <ramageem@oregonstate.edu>
2026-02-21 13:18:00 -0800

    initiate videoDemo.py template

---
+++ videoDemo.py
@@ -0,0 +1,31 @@
+# Reminders Microservice
+# Video Demo Program
+
+# *Test program programmatically requesting data from the microservice.
+
+# *The microservice programmatically responding with data.
+
+# *Test program programmatically receiving data from the microservice.
+
+# *Test program and microservice are not directly calling each other (show code).
+
+
+
+
+
+
+
+
+
+
+
+# *README describes how to request data from the microservice.
+# *README has an example call for requesting data.
+# *README describes how to receive data from the microservice.
+# *README has an example call for receiving data.
+# *README has a UML sequence diagram that communicates how to request and receive data from the microservice.
+
+
+
+
+# *UML diagram has no obvious notational errors.
