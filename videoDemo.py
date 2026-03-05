4ffe8044476c295570b3532b8d56e1511886970b
Emily Ramage <ramageem@oregonstate.edu>
2026-03-05 14:51:14 -0800

    removed unneeded comments from video planning

--- videoDemo.py
+++ videoDemo.py
@@ -29,19 +29,3 @@ time.sleep(3)
 # *Test program programmatically receiving data from the microservice.
 print("\nAll reminders:", response.json())
 # TEST PROGRAM SHOWS: <all reminders as json object>
-
-
-# *Test program and microservice are not directly calling each other (show code).
-# TEST PROGRAM: Show no imports, no function calls to microservice code.
-# MICROSERVICE: Show no imports, no function calls to test program code.
-# SHOW WEB BROWSER AT ENDPOINT: http://localhost:8000/reminders
-
-# **********************************************************************
-
-# *README describes how to request data from the microservice.
-# *README has an example call for requesting data.
-# *README describes how to receive data from the microservice.
-# *README has an example call for receiving data.
-# *README has a UML sequence diagram that communicates how to request and receive data from the microservice.
-
-# *UML diagram has no obvious notational errors.
